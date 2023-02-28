f0d722617862a4c5784130d995e0051a0201f99a
Jeroen Wilmes <jeroen.wilmes@vistacollege.nl>
2023-02-28 19:09:48 +0100

    update

--- testajax/functions.js
+++ testajax/functions.js
@@ -1,4 +1,14 @@
-// testfunctie voor asynchrone tekst
-function nieuweTekst(){
-    
-}
+function loadDoc() {
+    var xhttp = new XMLHttpRequest(); // request object instatieren
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        // 4 betekent "request finished, response ready", 200=status OK
+        // Vul id "demo" met de tekst uit bestand
+       document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    // welk type request en waarvandaan
+    xhttp.open("GET", "ajax_info.txt", true);
+    // verstuur de request naar de server
+    xhttp.send();
+  }
---
+++ testajax/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- haal js functie(s) binnen -->
+  <script src="functions.js"></script>
+</head>
+<body>
+<!-- Definier sectie met naam demo die door knop wordt gewijzigd -->
+<div id="demo">
+  <h2>Let AJAX change this text</h2>
+  <!-- met de knop wordt een js functie geladen -->
+  <button type="button" onclick="loadDoc()">Verander inhoud</button>
+</div>
+
+</body>
+</html>
